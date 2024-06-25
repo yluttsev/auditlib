29965691ceb5df271eb1a5dcacc995933fdc3e6d
Yuri Luttsev <floomyz@vk.com>
2024-06-25 13:14:10 +0300

    init

---
+++ src/main/java/ru/luttsev/auditlib/AuditlibApplication.java
@@ -0,0 +1,13 @@
+package ru.luttsev.auditlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditlibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditlibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/luttsev/auditlib/AuditlibApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.luttsev.auditlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditlibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
