e244deccb8eb58618474359b65e4bbc24c2dc7f8
Yuri Luttsev <floomyz@vk.com>
2024-06-27 19:18:56 +0300

    build: исправлена сборка библиотеки

--- spring-boot-starter-auditlib/src/test/java/ru/luttsev/springbootstarterauditlib/SpringBootStarterAuditlibApplicationTests.java
+++ spring-boot-starter-auditlib/src/test/java/ru/luttsev/springbootstarterauditlib/SpringBootStarterAuditlibApplicationTests.java
@@ -2,8 +2,9 @@ package ru.luttsev.springbootstarterauditlib;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.luttsev.springbootstarterauditlib.config.AuditLibAutoConfiguration;
 
-@SpringBootTest
+@SpringBootTest(classes = {AuditLibAutoConfiguration.class})
 class SpringBootStarterAuditlibApplicationTests {
 
     @Test
