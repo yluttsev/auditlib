fbc5a0cfd2c0ba61f90c8e20db19d61a94e563ee
Yuri Luttsev <floomyz@vk.com>
2024-07-12 18:28:10 +0300

    test: исправлен тест контекста

--- spring-boot-starter-auditlib/src/test/java/ru/luttsev/springbootstarterauditlib/SpringBootStarterAuditlibApplicationTests.java
+++ spring-boot-starter-auditlib/src/test/java/ru/luttsev/springbootstarterauditlib/SpringBootStarterAuditlibApplicationTests.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.luttsev.springbootstarterauditlib.config.AuditLibAutoConfiguration;
 
-@SpringBootTest(classes = {AuditLibAutoConfiguration.class})
+@SpringBootTest(
+        classes = {AuditLibAutoConfiguration.class},
+        properties = {
+                "auditlib.appender=console",
+                "auditlib.level=INFO"
+        })
 class SpringBootStarterAuditlibApplicationTests {
 
     @Test
